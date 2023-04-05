6cebe44540b8f308c80f7c7b3b7462523ed48fbe
DavideAtzo <atzo9477@gmail.com>
2023-04-05 15:56:50 +0200

    creazione tabella fatta

--- index.php
+++ index.php
@@ -69,10 +69,10 @@ var_dump($hotels);
         foreach ($hotels as $key => $hotel) { ?>
             <tr>
                 <td> <?php echo $hotel['name']; ?></td>
-                <td> <?php echo $hotel['name']; ?></td>
-                <td> <?php echo $hotel['name']; ?></td>
-                <td> <?php echo $hotel['name']; ?></td>
-                <td> <?php echo $hotel['name']; ?></td>
+                <td> <?php echo $hotel['description']; ?></td>
+                <td> <?php echo $hotel['parking']; ?></td>
+                <td> <?php echo $hotel['vote']; ?></td>
+                <td> <?php echo $hotel['distance_to_center']; ?></td>
             </tr>
         <?php } ?>
     </table>
